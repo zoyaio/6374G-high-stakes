1f52ca1ee169a3901d985f8eb43f325c9951cc62
JasmineLim559516 <142464324+JasmineLim559516@users.noreply.github.com>
2024-07-26 15:31:02 -0700

    Update intake.hpp

--- include/intake.hpp
+++ include/intake.hpp
@@ -1,7 +1,22 @@
 #pragma once
 #include "api.h"
+
+
 class Intake {
     private:
-    public: 
+        //intake constructor
+        pros::Motor intakeMotor;
+        pros::Motor intakeMotor2;
+
+        bool current_direction = true; //positive direction
+        
+    public:
+        //getters and setters
+        void set_voltage(int32_t voltage); //motor moves at this voltage
+        int32_t get_avg_velocity();
+
+        void set_direction(bool direction); //true is positive, false is negative
+        bool get_direction();
+        void switch_direction();
 };
 
