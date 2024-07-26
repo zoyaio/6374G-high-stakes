7ffa1faf7e40cae66f80ac7993e4ce06e883f8e4
JasmineLim559516 <142464324+JasmineLim559516@users.noreply.github.com>
2024-07-26 15:32:36 -0700

    Update Robot.hpp

--- include/Robot.hpp
+++ include/Robot.hpp
@@ -1,12 +1,39 @@
 #pragma once
 #include "api.h"
+#include "intake.cpp"
+#include "drivetrain.cpp"
 
 
 class Robot {
     private:
         pros::Controller m_controller {pros::E_CONTROLLER_MASTER}; 
 
- 
-    public: 
+        //robot constructor
+        Robot(Drivetrain drive, Intake intake);
 
+        //robot components
+        Intake intake;
+        Drivetrain dt;
+
+        //update robot components
+        void update_intake(){
+            //intakes rings
+            if (m_controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1) && intake.get_avg_velocity() == 0) {
+                intake.set_direction(true); //spins intake in positive direction
+                intake.set_voltage(12000); //spins at max voltage
+            }
+            if (m_controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1) && intake.get_avg_velocity() != 0) {
+                intake.set_voltage(0); //turns off intake
+            }
+            //deintakes rings
+            if (m_controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+                intake.set_direction(false); //spins intake in opposite direction
+                intake.set_voltage(12000);
+            }
+        }
+        void update_drivetrain();
+
+    public: 
+        //update function for whole robot
+        void update(std::string info);
 };
